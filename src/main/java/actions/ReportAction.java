9f16891ecf9f81b927f020c31d720681e8c6b7a3
ykosh <ykosh@DESKTOP-FU152O7>
2021-12-08 22:15:52 +0900

    日報indexアクションの作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,93 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+/**
+ * 日報に関する処理を行うActionクラス
+ */
+public class ReportAction extends ActionBase {
+
+    private ReportService service;
+
+    /**
+     * メソッドを実行する
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new ReportService();
+
+        //メソッドを実行
+        invoke();
+        service.close();
+    }
+
+    /**
+     * 一覧画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void index() throws ServletException, IOException{
+
+        //指定されたページ数の一覧画面に表示する日報データを取得
+        int page = getPage();
+        List<ReportView> reports = service.gettAllPerPage(page);
+
+        //全日報データの件数を取得
+        long reportsCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+
+        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移す
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if(flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //一覧画面を表示
+        forward(ForwardConst.FW_REP_INDEX);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
